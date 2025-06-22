039664ff96b0cf1bd8d528bdc862dc692eb6dcc4
Edmilson Monteiro Rodrigues Neto <edmilson.rodriguesn38@gmail.com>
2025-06-22 09:25:47 +0100

    feat: add base project structure

--- src/report_generator/app.py
+++ src/report_generator/app.py
@@ -1,5 +1,6 @@
 """
-This is an application used to generate quality assurance reports for enteral feeding products
+This is an application used to generate quality assurance reports for enteral
+feeding products.
 """
 
 import importlib.metadata
@@ -8,25 +9,16 @@ import sys
 from PySide6 import QtWidgets
 
 
-class ReportGenerator(QtWidgets.QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.init_ui()
-
-    def init_ui(self):
-        self.setWindowTitle('report_generator')
-        self.show()
-
-
 def main():
-    # Linux desktop environments use an app's .desktop file to integrate the app
-    # in to their application menus. The .desktop file of this app will include
-    # the StartupWMClass key, set to app's formal name. This helps associate the
-    # app's windows to its menu item.
+    # ###################### BRIEFCASE EXPLANATION #########################
+    # Linux desktop environments use an app's .desktop file to integrate the
+    # app in to their application menus. The .desktop file of this app will
+    # include the StartupWMClass key, set to app's formal name. This helps
+    # associate the app's windows to its menu item.
     #
-    # For association to work, any windows of the app must have WMCLASS property
-    # set to match the value set in app's desktop file. For PySide6, this is set
-    # with setApplicationName().
+    # For association to work, any windows of the app must have WMCLASS
+    # property set to match the value set in app's desktop file. For PySide6,
+    # this is set with setApplicationName().
 
     # Find the name of the module that was used to start the app
     app_module = sys.modules['__main__'].__package__
@@ -36,5 +28,4 @@ def main():
     QtWidgets.QApplication.setApplicationName(metadata['Formal-Name'])
 
     app = QtWidgets.QApplication(sys.argv)
-    main_window = ReportGenerator()
     sys.exit(app.exec())
